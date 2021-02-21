867689d0c81fa340dedf707cbbffdc6e9e68fa27
Goksal TAK <goksaltak@hotmail.com>
2021-02-22 01:37:29 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Listelendi :" + musteri.Ad + " " + musteri.Soyad + " " + musteri.DogumYeri);
+            
+        }
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi :" + musteri.Ad + " " + musteri.Soyad +" " + musteri.DogumYeri);
+           
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi :" + musteri.Ad + " " + musteri.Soyad + " " + musteri.DogumYeri);
+            
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,47 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ahmet";
+            musteri1.Soyad = "Yıldırım";
+            musteri1.DogumYeri = "Beykoz";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Engin";
+            musteri2.Soyad = "Albayrak";
+            musteri2.DogumYeri = "Çaykara";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Göksal";
+            musteri3.Soyad = "TAK";
+            musteri3.DogumYeri = "Bakırköy";
+
+            Musteri[] musterilistele = new Musteri[] { musteri1, musteri2, musteri3 };
+            MusteriManager musteriManager = new MusteriManager();
+            foreach (var musteriler in musterilistele)
+            {
+                musteriManager.Listele(musteriler);
+            }
+            Console.WriteLine("-----------------Liste Sonu--------------------");
+            
+
+            Musteri[] musteriekle = new Musteri[] { musteri1, musteri2,musteri3 };
+            foreach (var musteriler in musteriekle)
+            {
+                musteriManager.Ekle(musteriler);
+            }
+            Console.WriteLine("-----------------Ekleme Sonu--------------------");
+            Musteri[] musterisil = new Musteri[] { musteri1};
+            foreach (var musteriler in musterisil)
+            {
+                musteriManager.Sil(musteriler);
+            }
+
+            Console.WriteLine("-----------------Silme Sonu--------------------");
+
+
+
+
         }
     }
 }
